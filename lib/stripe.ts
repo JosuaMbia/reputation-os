4505b7a20490ba45402091fd026b4f5086959beb
JosuaMbia <josuambia75@gmail.com>
2025-12-18 21:22:40 +0000

    fix: stripe api version mismatch

--- lib/stripe.ts
+++ lib/stripe.ts
@@ -1,6 +1,7 @@
 import Stripe from 'stripe';
 
 export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: '2025-01-27.acacia', // Ou la version la plus récente suggérée par VS Code
+  // On utilise 'as any' pour éviter que TypeScript ne bloque sur la version exacte
+  apiVersion: '2025-12-15.clover' as any, 
   typescript: true,
 });
